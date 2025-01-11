53425d5556a4fc13bd424aeac90f6eff26e2a878
whoIsBeto <roberto.cortes@techholding.co>
2025-01-10 23:11:54 -0600

    [remove] postcss, autoprefixer and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Provider } from "@/components/ui/provider.tsx";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import "./next-i18next.config.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
