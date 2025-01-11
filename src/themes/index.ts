4a07344d971908faed4b514cdc1ab1bd74e3d799
LaloStroke <drak_lost@hotmail.com>
2025-01-11 01:02:45 -0600

    [add] Prettier config.

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -4,8 +4,8 @@ import {
   createSystem,
   defaultConfig,
   defineConfig,
-  mergeConfigs
-} from "@chakra-ui/react";
+  mergeConfigs,
+} from '@chakra-ui/react';
 
 // 2. Call `extendTheme` and pass your custom values
 const theme = defineConfig({
@@ -13,38 +13,38 @@ const theme = defineConfig({
     tokens: {
       colors: {
         brandGreen: {
-          DEFAULT: { value: "#ABF510" },
-          100: { value: "#96D90E" },
-          200: { value: "#87BE0C" },
-          300: { value: "#73A608" }
+          DEFAULT: { value: '#ABF510' },
+          100: { value: '#96D90E' },
+          200: { value: '#87BE0C' },
+          300: { value: '#73A608' },
         },
         brandBlue: {
-          DEFAULT: { value: "#115FF2" },
-          100: { value: "#E6EEFE" }
+          DEFAULT: { value: '#115FF2' },
+          100: { value: '#E6EEFE' },
         },
         brandBlack: {
-          DEFAULT: { value: "#000000" },
-          100: { value: "#414045" },
-          200: { value: "#EDF0EF" },
-          300: { value: "#F3F6F6" }
+          DEFAULT: { value: '#000000' },
+          100: { value: '#414045' },
+          200: { value: '#EDF0EF' },
+          300: { value: '#F3F6F6' },
         },
         brandWhite: {
-          DEFAULT: { value: "#FFFFFF" }
-        }
-      }
+          DEFAULT: { value: '#FFFFFF' },
+        },
+      },
     },
     textStyles: {
       body: {
-        fontFamily: "Nats sans-serif"
-      }
+        fontFamily: 'Nats sans-serif',
+      },
     },
     layerStyles: {
       base: {
-        backgroundColor: "brandWhite",
-        color: "brandBlack.100"
-      }
-    }
-  }
+        backgroundColor: 'brandWhite',
+        color: 'brandBlack.100',
+      },
+    },
+  },
 });
 
 const config = mergeConfigs(defaultConfig, theme);
