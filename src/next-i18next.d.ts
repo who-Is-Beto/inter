5732597c4f6cc4d4108b232c056b69dae6228754
LaloStroke <drak_lost@hotmail.com>
2025-01-14 04:47:07 -0600

    [fix] resolve conflict to merge main

--- src/next-i18next.d.ts
+++ src/next-i18next.d.ts
@@ -1,13 +1,13 @@
 // import all namespaces (for the default language, only)
-import es from "./locales/es.json";
-import en from "./locales/en.json";
+import es from './locales/es.json';
+import en from './locales/en.json';
 
 type ES = typeof es;
 type EN = typeof en;
 
-declare module "i18next" {
+declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: "translation";
+    defaultNS: 'translation';
     resources: {
       translation: ES;
     };
