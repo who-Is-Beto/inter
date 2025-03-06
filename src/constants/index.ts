b7e8649491c6591b7ea064555cd8677e80c1f8b7
whoIsBeto <whoisbeto@gmail.com>
2025-03-06 09:34:30 -0600

    [resolve] conflicts with master branch

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,14 +2,18 @@ export const LANGUAGES = [
   { label: 'Spanish', code: 'es' },
   { label: 'English', code: 'en' },
   { label: 'French', code: 'fr' },
+  { label: 'Spanish', code: 'es' },
+  { label: 'English', code: 'en' },
+  { label: 'French', code: 'fr' },
 ];
 
-export const Routes = {
-  home: '/',
-  login: '/login',
-  register: '/register',
-  dashboard: '/dashboard',
-  tienda: '/tienda',
-  config: '/config',
-  signup: '/SignUp',
-};
+export enum Routes {
+  home = '/',
+  login = '/login',
+  register = '/register',
+  dashboard = '/dashboard',
+  tienda = '/tienda',
+  config = '/config',
+}
+
+export const BaseURL = import.meta.env.VITE_BaseURL;
